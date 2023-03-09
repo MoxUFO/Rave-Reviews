9cc2c7a47c57ce19fb1cc25ae425258db2b5d5e3
Nathan Moon <moonman700@gmail.com>
2023-03-09 18:38:40 -0500

    work in progress for API calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,3 +37,101 @@ $("#inverted_calendar").calendar({
 });
 
 searchBtn.addEventListener("click", handleEventQuery);
+
+// variables
+var key = "wAUd3TteUrno75GAGlIlbBXcvGgQurTA"
+let cityName = ""
+let venueAddress = ""
+let venueName = ""
+let urlVenueName = ""
+let ticketMasterReturn = {}
+let eventInfo = {}
+let tempInfo = {}
+let step = 0
+yelpAPIreturn = {}
+
+TicketMasterAPIcall()
+
+
+// initial Ticketmaster API call
+function TicketMasterAPIcall () {
+  cityName = prompt("Please enter a city")
+  fetch(`https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=${key}&city=${cityName}`)
+      .then(function(data) {
+          return(data.json())        
+      })
+      .then(function(data) {
+          ticketMasterReturn = data
+          // console.log(data)
+          for (i = 0; i < 10; i++) {
+              tempInfo = {}
+              // finds the event name
+              tempInfo['EventName'] = data._embedded.events[i].name
+
+              // finds the venue name
+              venueName = data._embedded.events[i]._embedded.venues[0].name
+              tempInfo['VenueName'] = data._embedded.events[i]._embedded.venues[0].name
+
+              // finds the event address
+              venueAddress = data._embedded.events[i]._embedded.venues[0].address.line1
+              tempInfo['EventAddress'] = data._embedded.events[i]._embedded.venues[0].address.line1
+            
+              // Event start time
+              tempInfo['EventStart'] = data._embedded.events[i].dates.start.localTime
+
+              // Event Date
+              tempInfo['EventDate'] = data._embedded.events[i].dates.start.localDate
+
+              // Ticket price range
+              tempInfo['EventPrices'] = [data._embedded.events[i].priceRanges[0].min, data._embedded.events[i].priceRanges[0].max]
+
+              // Ticket status
+              tempInfo['EventStatus'] = data._embedded.events[i].dates.status.code
+
+              // Genre
+              tempInfo['EventGenre'] = data._embedded.events[i].classifications[0].genre.name
+              
+              eventInfo[i] = tempInfo            
+          }
+          encodeVariables()   
+      })
+}      
+// for each search result, perform an yelp API call using the encoded venue name
+function encodeVariables() {
+  for (i=0; i < 10; i++) {  
+    // encodes address to function within url 
+    tempURLname = eventInfo[i].VenueName.replace(/\s/g, '%20')    
+    // calls yelp api
+    yelpAPIcall()  
+  }
+}
+
+// compares address from TicketMaster to Yelp and attempt to find a matching rating
+function yelpAPIfilter() {
+  failedYelpURL = yelpURL
+  tempAddress = eventInfo[step].EventAddress
+  tempAddress1 = eventInfo[step].EventAddress.replace(/\./g, "")
+  tempAddress2 = eventInfo[step].EventAddress.replace(/Drive/g, 'Dr')
+  for (j = 0; j < yelpAPIreturn.businesses.length; j++) {
+    // console.log(yelpAPIreturn.businesses[j].location.address1 + " " + tempAddress + " " + tempAddress1 + " " + tempAddress2)
+    if (tempAddress == yelpAPIreturn.businesses[j].location.address1 || tempAddress1 == yelpAPIreturn.businesses[j].location.address1 || tempAddress2 == yelpAPIreturn.businesses[j].location.address1) {
+      eventInfo[step].rating = yelpAPIreturn.businesses[j].rating
+      break   
+    }   
+  }
+  // increases step count by 1
+  step = step + 1
+  console.log(eventInfo)
+}
+
+// calls yelp api using encoded venue name
+function yelpAPIcall() {
+  yelpURL = `https://yelp-backend.netlify.app/.netlify/functions/search?term=${tempURLname}&location=${cityName}`
+  fetch(yelpURL, {})
+    .then((res) => {return res.json()})
+    .then((data) => {
+      yelpAPIreturn = data
+      // calls the yelpAPIfilter function that parses the data
+      yelpAPIfilter()
+      })
+}
