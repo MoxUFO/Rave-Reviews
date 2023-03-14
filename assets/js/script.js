4d4ba01e8a8b5826cc9d5fe94ef27f44955ca205
MOX UFO <mox.ufo@gmail.com>
2023-03-13 23:07:02 -0400

    button now fires on most clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ let eventLocation = document.getElementById("location-input")
 let dateInput = document.getElementById("date-input")
 let eventType = document.getElementById("event-type")
 let genreList = document.getElementById("genre-list")
+let searchBtn = document.getElementById("search-events");
 
 $('#inverted_calendar')
   .calendar({
@@ -13,7 +14,52 @@ $('#inverted_calendar')
     }
   });
 
-let searchBtn = document.getElementById("search-events");
+  function genreChange(event) {
+    
+    let eventType = document.getElementById("event-type").value;
+    if (eventType == 'music') {
+    let alternative = document.createElement('option');
+    let childrens = document.createElement('option');
+    let classical = document.createElement('option');
+    let country = document.createElement('option');
+    let dance = document.createElement('option');
+    let rap = document.createElement('option');
+    let latin = document.createElement('option');
+    let metal = document.createElement('option');
+    let pop = document.createElement('option');
+    let rnb = document.createElement('option');
+    let religious = document.createElement('option');
+    let rock = document.createElement('option');
+    
+    alternative.text = "alternative";
+    childrens.text = "children's";
+    classical.text = "classical";
+    country.text = "country";
+    dance.text = "dance/electronic";
+    rap.text = "hip-hop/rap";
+    latin.text = "Latin";
+    metal.text = "metal";
+    pop.text = "pop";
+    rnb.text = "R&B";
+    religious.text = "religious";
+    rock.text = "rock";
+  
+    document.getElementById('genre-list').append(alternative);
+    document.getElementById('genre-list').append(childrens);
+    document.getElementById('genre-list').append(classical);
+    document.getElementById('genre-list').append(country);
+    document.getElementById('genre-list').append(dance);
+    document.getElementById('genre-list').append(rap);
+    document.getElementById('genre-list').append(metal);
+    document.getElementById('genre-list').append(pop);
+    document.getElementById('genre-list').append(rnb);
+    document.getElementById('genre-list').append(religious);
+    document.getElementById('genre-list').append(rock);
+    document.getElementById('genre-list').append(world);
+    }
+  }
+
+
 
 function handleEventQuery() {
   eventParent.innerHTML = " "
@@ -25,6 +71,7 @@ function handleEventQuery() {
   dateInput = dateInput + "T00:00:00Z"
   }
   let eventType = document.getElementById("event-type").value;
+
   let genreList = document.getElementById("genre-list").value;
   introDiv.classList.add('hide-me')
   eventParent.classList.remove('hide-me')
@@ -32,13 +79,14 @@ function handleEventQuery() {
 }
 
 function eventDetails(event){
-  console.log(event.target.children[4].innerHTML)
-  if (event.target.children[4].innerHTML == undefined){
-  } else{
-    queryString = './results.html?id=' + event.target.children[4].innerHTML
+  console.log(event.target)
+  // if (event.target.children[4].innerHTML == undefined){
+  // } else{
+    console.log(event.target.parentElement)
+    queryString = './results.html?id=' + event.target.parentElement.dataset.eventId
     location.assign(queryString);
     console.log(queryString)
-  }
+  // }
 }
 
 function TicketMasterAPIcall(eventLocation, dateInput, eventType,genreList){
@@ -68,12 +116,18 @@ function TicketMasterAPIcall(eventLocation, dateInput, eventType,genreList){
       let eventHeader = document.createElement('h3')
       eventHeader.textContent = data._embedded.events[i].name
       let genreDiv = document.createElement('div')
-      genreDiv.textContent = data._embedded.events[i].classifications[0].genre.name
+      if (!data._embedded.events[i].classifications[0].genre) {
+        genreDiv.textContent = 'undefined'
+      } else{
+        genreDiv.textContent = data._embedded.events[i].classifications[0].genre.name
+      }
       let venueDiv = document.createElement('div')
       venueDiv.textContent = data._embedded.events[i]._embedded.venues[0].name
       let dateDiv = document.createElement('div')
       dateDiv.textContent = data._embedded.events[i].dates.start.localDate + ' ' + data._embedded.events[i].dates.start.localTime
-      let eventId = document.createElement('div')
+      contentDiv.dataset.eventId = data._embedded.events[i].id
+      imageDiv.dataset.eventId = data._embedded.events[i].id
+      console.log(eventCard.dataset.eventId)
       let thumbnail = document.createElement('img')
       let biggestImg = {}
       biggestImg['height'] = 0
@@ -88,26 +142,26 @@ function TicketMasterAPIcall(eventLocation, dateInput, eventType,genreList){
       }        
       thumbnail.src = biggestImg['Url']
       thumbnail.id = "thumbnail"
-      console.log(data)
+      // console.log(eventCard)
       
-      eventId.textContent = data._embedded.events[i].id
-      eventId.classList.add('hide-me')
+      // eventId.textContent = data._embedded.events[i].id
+      // eventId.classList.add('hide-me')
 
-      contentDiv.append(eventHeader)
-      contentDiv.append(genreDiv)
-      contentDiv.append(venueDiv)
-      contentDiv.append(dateDiv)
-      contentDiv.append(eventId)
+      contentDiv.appendChild(eventHeader)
+      contentDiv.appendChild(genreDiv)
+      contentDiv.appendChild(venueDiv)
+      contentDiv.appendChild(dateDiv)
+      // contentDiv.appendChild(eventId)
       
      
 
-      eventDiv.append(eventCard)
+      eventDiv.appendChild(eventCard)
 
 
-      imageDiv.append(thumbnail)
+      imageDiv.appendChild(thumbnail)
 
 
-      eventParent.append(eventDiv)
+      eventParent.appendChild(eventDiv)
       eventCard.addEventListener('click', eventDetails)
       
    
@@ -118,4 +172,5 @@ function TicketMasterAPIcall(eventLocation, dateInput, eventType,genreList){
      }
     });
    }
+   eventType.addEventListener('change',genreChange)
 searchBtn.addEventListener("click", handleEventQuery);
